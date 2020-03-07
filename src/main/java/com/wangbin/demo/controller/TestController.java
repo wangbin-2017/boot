66de54771c5df4e94b117fdfaaf8b28e52ae0790
wangbin <wangbin5498@qq.com>
2020-03-08 00:40:51 +0800

    v1

---
+++ src/main/java/com/wangbin/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.wangbin.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "123123123";
+    }
+}
